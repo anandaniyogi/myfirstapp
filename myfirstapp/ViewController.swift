be5dd625621fc9e566cac50cec4636e9e1003f47
Ananda Niyogi <anandaniyogi@Anandas-MacBook-Pro.local>
2016-02-25 21:13:51 +0530

    My Second Manual Commit

--- myfirstapp/ViewController.swift
+++ myfirstapp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageOutlet: UIImageView!
+    @IBOutlet weak var TextOutlet: UITextField!
+    @IBOutlet weak var MainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func OnClickMethod(sender: AnyObject) {
+        
+        ImageOutlet.hidden = false
+        TextOutlet.hidden = false
+        MainButton.hidden = true
+        
+    }
+    
+    
 
 }
 
